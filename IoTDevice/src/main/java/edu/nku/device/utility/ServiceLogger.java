9bd07baa7606497f2850a120fc2325df3b561a8c
llykowski <lykowskil1@nku.edu>
2017-04-09 14:50:09 -0400

    Initial Commit of IoTDevice

---
+++ IoTDevice/src/main/java/edu/nku/device/utility/ServiceLogger.java
@@ -0,0 +1,27 @@
+package edu.nku.device.utility;
+
+import java.sql.Timestamp;
+
+public class ServiceLogger {
+	/*
+	 * A singleton class to centralize disk access to log files.
+	 */
+
+	private static ServiceLogger instance = null;
+
+	private ServiceLogger() {
+	}
+
+	public static ServiceLogger getInstance() {
+		if (instance == null) {
+			instance = new ServiceLogger();
+		}
+		return instance;
+	}
+
+	public void writeLog(String x) {
+		Timestamp time = new Timestamp(System.currentTimeMillis());
+		// TODO: Write log files to file system rather than to the console.
+		System.out.println("[" + time + "]" + ":" + x);
+	}
+}
