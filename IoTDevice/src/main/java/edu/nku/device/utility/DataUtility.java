8165e6e2d9a28076c5195c759e77b5c63845535d
llykowski <lykowskil1@nku.edu>
2017-04-17 21:10:10 -0400

    Changes to build into executable jar

--- IoTDevice/src/main/java/edu/nku/device/utility/DataUtility.java
+++ IoTDevice/src/main/java/edu/nku/device/utility/DataUtility.java
@@ -45,7 +45,9 @@ public class DataUtility {
 		}
 
 		try {
-			this.conn = DriverManager.getConnection("jdbc:sqlite:" + this.dbName);
+			//this.conn = DriverManager.getConnection("jdbc:sqlite:" + this.dbName);
+			this.conn = DriverManager.getConnection("jdbc:sqlite::resource:" + 
+			        getClass().getResource("/" + dbName));
 		} catch (SQLException e) {
 			logger.writeLog("DataUtility.getConnection() - Could not get connection.");
 			e.printStackTrace();
